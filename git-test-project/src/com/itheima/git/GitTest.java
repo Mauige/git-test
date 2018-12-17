0f27789928db6b49af1fd864f1d00a75973e1e80
admin <admin@itcast.cn>
2018-12-17 20:45:17 +0800

    添加工程至git

--- git-test-project/src/com/itheima/git/GitTest.java
+++ git-test-project/src/com/itheima/git/GitTest.java
@@ -4,4 +4,11 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("hello git project ");
     }
+
+    public void test(){
+
+        System.out.println("第二个方法test执行");
+    }
+
+
 }
